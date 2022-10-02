b2b66aa0c4ea2f0dfd591ef8bf24ad20c3f6e8ad
Nick (Git client) <nick@cassiel.com>
2022-10-02 11:48:09 +0100

    Quick test of Sysex for LCD display.

--- python/run-native.sh
+++ python/run-native.sh
@@ -3,6 +3,11 @@
 ### workon motor-mix
 
 python console-native.py \
-       --from_mm="USB.*1" \
-       --to_mm="USB.*1" \
+       --from_mm="USB.*Port 1" \
+       --to_mm="USB.*Port 1" \
        --output="IAC.*1"
+
+#python console-native.py \
+#       --from_mm="iConnect.*DIN 1" \
+#       --to_mm="iConnect.*DIN 1" \
+#       --output="IAC.*1"
